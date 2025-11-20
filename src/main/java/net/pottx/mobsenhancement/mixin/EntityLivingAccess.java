688bf81d39a81f0f5e40c7adfc303036bf430335
Abigail Read <AbbyReadMail@gmail.com>
2025-11-19 19:29:24 -0800

    Merge EntityLivingAccess

--- src/main/java/net/pottx/mobsenhancement/mixin/EntityLivingAccess.java
+++ src/main/java/net/pottx/mobsenhancement/mixin/EntityLivingAccess.java
@@ -2,11 +2,19 @@ package net.pottx.mobsenhancement.mixin;
 
 import net.minecraft.src.*;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(EntityLiving.class)
 public interface EntityLivingAccess {
+
+    @Unique
+    boolean realisticCanEntityBeSeen(Entity entity, double absDist);
+
+    @Unique
+    boolean realisticCanEntityBeSensed(Entity entity);
+
     @Invoker("addRandomArmor")
     void invokeAddRandomArmor();
 
