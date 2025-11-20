9ba0b89a361bcf64e1ed930533a08dc380d4a5a2
Abigail Read <AbbyReadMail@gmail.com>
2025-11-20 00:08:51 -0800

    Let difficulty determine search range

--- src/main/java/net/pottx/mobsenhancement/mixin/EntityLivingMixin.java
+++ src/main/java/net/pottx/mobsenhancement/mixin/EntityLivingMixin.java
@@ -7,9 +7,6 @@ import net.minecraft.src.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(EntityLiving.class)
 public abstract class EntityLivingMixin extends Entity implements EntityLivingAccess {
@@ -20,14 +17,6 @@ public abstract class EntityLivingMixin extends Entity implements EntityLivingAc
         super(par1World);
     }
 
-    @Inject(
-            method = "func_96121_ay()I",
-            at = @At(value = "HEAD"),
-            cancellable = true
-    )
-    private void returnBiggerPathSearchRange(CallbackInfoReturnable<Integer> cir) {
-        cir.setReturnValue(32);
-    }
 
     @Inject(
             method = "canEntityBeSeen(Lnet/minecraft/src/Entity;)Z",
