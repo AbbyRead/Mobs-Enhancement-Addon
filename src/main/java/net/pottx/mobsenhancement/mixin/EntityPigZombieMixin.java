0cc93eae9c3ef61c49e0a0a4bc86fe34440de410
Abigail Read <AbbyReadMail@gmail.com>
2025-11-20 07:18:53 -0800

    Reimplement progression-based zombie pig men hostility

--- src/main/java/net/pottx/mobsenhancement/mixin/EntityPigZombieMixin.java
+++ src/main/java/net/pottx/mobsenhancement/mixin/EntityPigZombieMixin.java
@@ -5,10 +5,11 @@ import net.minecraft.src.EntityPlayer;
 import net.minecraft.src.EntityZombie;
 import net.minecraft.src.World;
 import net.pottx.mobsenhancement.MEAUtils;
+import net.pottx.mobsenhancement.mixin.access.EntityPigZombieAccess;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(EntityPigZombie.class)
-public abstract class EntityPigZombieMixin extends EntityZombie {
+public abstract class EntityPigZombieMixin extends EntityZombie implements EntityPigZombieAccess {
     public EntityPigZombieMixin(World par1World) {
         super(par1World);
     }
@@ -19,7 +20,7 @@ public abstract class EntityPigZombieMixin extends EntityZombie {
 
         float f = this.worldObj == null ? 1.5F : MEAUtils.getGameProgressMobsLevel(this.worldObj) > 1 ? 6.0F : 1.5F;
 
-        EntityPlayer closestPlayer = this.worldObj.getClosestPlayerToEntity(this, f);
+        EntityPlayer closestPlayer = this.worldObj == null ? null : this.worldObj.getClosestPlayerToEntity(this, f);
 
         if (closestPlayer != null) {
             this.becomeAngryAt(closestPlayer);
