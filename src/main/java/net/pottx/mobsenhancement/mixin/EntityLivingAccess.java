c13999b92128f1301389d4dab3ea9be2441c4711
Abigail Read <AbbyReadMail@gmail.com>
2025-11-20 03:27:38 -0800

    Remove unused import

--- src/main/java/net/pottx/mobsenhancement/mixin/EntityLivingAccess.java
+++ src/main/java/net/pottx/mobsenhancement/mixin/EntityLivingAccess.java
@@ -2,7 +2,6 @@ package net.pottx.mobsenhancement.mixin;
 
 import net.minecraft.src.*;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
