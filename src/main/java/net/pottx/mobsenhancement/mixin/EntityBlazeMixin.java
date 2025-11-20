f22a4995e6d25f44dea4befa831c2d6c76ac7084
Abigail Read <AbbyReadMail@gmail.com>
2025-11-19 16:59:30 -0800

    Fix override by making it an inject into an initialization method

--- src/main/java/net/pottx/mobsenhancement/mixin/EntityBlazeMixin.java
+++ src/main/java/net/pottx/mobsenhancement/mixin/EntityBlazeMixin.java
@@ -2,9 +2,13 @@ package net.pottx.mobsenhancement.mixin;
 
 import net.minecraft.src.EntityBlaze;
 import net.minecraft.src.EntityLivingBase;
+import net.minecraft.src.SharedMonsterAttributes;
 import net.minecraft.src.World;
 import net.pottx.mobsenhancement.MEAUtils;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntityBlaze.class)
 public abstract class EntityBlazeMixin extends EntityLivingBase {
@@ -13,8 +17,24 @@ public abstract class EntityBlazeMixin extends EntityLivingBase {
         super(world);
     }
 
-    @Override
-    public int getMaxHealth() {
-        return this.worldObj == null ? 20 : MEAUtils.getGameProgressMobsLevel(this.worldObj) > 1 ? 24 : 20;
+    @Inject(method = "applyEntityAttributes", at = @At("RETURN"))
+    private void modifyMaxHealth(CallbackInfo ci) {
+        double baseHp = 20.0;
+
+        if (this.worldObj != null) {
+            int tier = MEAUtils.getGameProgressMobsLevel(this.worldObj);
+            if (tier > 1) {
+                baseHp = 24.0;
+            }
+        }
+
+        // Set new max HP
+        this.getEntityAttribute(SharedMonsterAttributes.maxHealth)
+                .setAttribute(baseHp);
+
+        // Heal Blaze to its new max HP since the super.applyEntityAttributes
+        //   call may have set its health lower, and applyEntityAttributes is
+        //   only used at construction time of the entity.
+        this.setHealth((float) baseHp);
     }
 }
