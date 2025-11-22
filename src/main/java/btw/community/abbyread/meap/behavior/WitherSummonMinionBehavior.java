54f711f70a8b1f0cb7c80610cde4ade69a2d14bd
Abigail Read <AbbyReadMail@gmail.com>
2025-11-22 13:45:48 -0800

    Re-do summon behavior 1:1 with MEA
    Updated to BTW 3.0.0

--- src/main/java/btw/community/abbyread/meap/behavior/WitherSummonMinionBehavior.java
+++ src/main/java/btw/community/abbyread/meap/behavior/WitherSummonMinionBehavior.java
@@ -23,133 +23,102 @@ public class WitherSummonMinionBehavior extends EntityAIBase {
 
     @Override
     public boolean shouldExecute() {
-        if (!myWither.isEntityAlive() || ((EntityWitherAccess)myWither).meap$getSpawnInvulnerabilityTime() > 0 || myWither.isArmored()) {
+        if (!this.myWither.isEntityAlive() || ((EntityWitherAccess)this.myWither).meap$getSpawnInvulnerabilityTime() > 0 || this.myWither.isArmored()) {
             return false;
         }
-
-        summonCooldownCounter--;
-        return myWither.getAttackTarget() != null && summonCooldownCounter <= 0;
+        this.summonCooldownCounter--;
+        return this.myWither.isEntityAlive() && this.myWither.getAttackTarget() != null &&
+                ((EntityWitherAccess)this.myWither).meap$getSpawnInvulnerabilityTime() <= 0 && !this.myWither.isArmored() && this.summonCooldownCounter <= 0;
     }
 
-    @Override
     public boolean continueExecuting() {
-        return summonProcessCounter > 0;
+        return this.summonProcessCounter > 0;
     }
 
-    @Override
     public void resetTask() {
-        summonCooldownCounter = 160 + myWither.rand.nextInt(80);
-        summonProcessCounter = 0;
-        ((EntityWitherExtend) myWither).meap$setIsDoingSpecialAttack(false);
+        super.resetTask();
+
+        this.summonCooldownCounter = 160 + this.myWither.rand.nextInt(80);
+        ((EntityWitherExtend)this.myWither).meap$setIsDoingSpecialAttack(false);
     }
 
-    @Override
     public void startExecuting() {
-        summonProcessCounter = 40; // Animation duration
-        ((EntityWitherExtend) myWither).meap$setIsDoingSpecialAttack(true);
-
-        int baseY = MathHelper.floor_double(myWither.posY);
-
-        for (int i = 0; i < MAX_SUMMONS; i++) {
-            boolean unique;
-            do {
-                unique = true;
-                summonX[i] = MathHelper.floor_double(myWither.posX) + myWither.rand.nextInt(9) - 4;
-                summonZ[i] = MathHelper.floor_double(myWither.posZ) + myWither.rand.nextInt(9) - 4;
-                // Check duplicates against previous summons
-                for (int j = 0; j < i; j++) {
-                    if (summonX[i] == summonX[j] && summonZ[i] == summonZ[j]) {
-                        unique = false;
+        this.summonProcessCounter = 40;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (i == 0) {
+                    summonX[i] = MathHelper.floor_double(this.myWither.posX) + this.myWither.rand.nextInt(9) - 4;
+                    summonZ[i] = MathHelper.floor_double(this.myWither.posZ) + this.myWither.rand.nextInt(9) - 4;
+                } else {
+                    boolean duplicated = true;
+                    while (duplicated) {
+                        for (int k = 0; k < i; k++) {
+                            summonX[i] = MathHelper.floor_double(this.myWither.posX) + this.myWither.rand.nextInt(9) - 4;
+                            summonZ[i] = MathHelper.floor_double(this.myWither.posZ) + this.myWither.rand.nextInt(9) - 4;
+                            if (summonX[i] != summonX[k] || summonZ[i] != summonZ[k]) {
+                                duplicated = false;
+                            }
+                        }
+                    }
+                }
+                int startingY = MathHelper.floor_double(this.myWither.posY);
+                summonY[i] = startingY;
+                while (summonY[i] > 0 && MathHelper.floor_double(this.myWither.posY) - summonY[i] < 64) {
+                    EntitySkeleton summonCheck = (EntitySkeleton) EntityList.createEntityByName("WitherSkeleton", this.myWither.worldObj);
+                    summonCheck.setSkeletonType(1);
+                    summonCheck.setPosition(summonX[i] + 0.5D, summonY[i], summonZ[i] + 0.5D);
+                    if (this.myWither.worldObj.checkNoEntityCollision(summonCheck.boundingBox) &&
+                            this.myWither.worldObj.getCollidingBoundingBoxes(summonCheck, summonCheck.boundingBox).isEmpty() &&
+                            this.myWither.worldObj.doesBlockHaveSolidTopSurface(summonX[i], summonY[i] - 1, summonZ[i]))
                         break;
+                    if (summonY[i] == startingY - 8) {
+                        summonY[i] = startingY + 6;
+                    } else if (summonY[i] == startingY + 1) {
+                        summonY[i] = startingY - 9;
+                    } else {
+                        summonY[i] -= 1;
                     }
                 }
-            } while (!unique);
-
-            summonY[i] = findValidGroundY(summonX[i], baseY, summonZ[i]);
-        }
-
-        // If no valid spawn locations, cancel summoning
-        boolean hasValidY = false;
-        for (int y : summonY) {
-            if (y > 0) {
-                hasValidY = true;
-                break;
+                if (summonY[i] > 0) break;
             }
         }
-        if (!hasValidY) {
-            summonProcessCounter = -1;
-            ((EntityWitherExtend) myWither).meap$setIsDoingSpecialAttack(false);
-            return;
+            if (summonY[0] > 0 || summonY[1] > 0 || summonY[2] > 0) {
+                this.myWither.worldObj.playSoundEffect(this.myWither.posX, this.myWither.posY, this.myWither.posZ, "mob.wither.idle", 2F, 0.25F);
+
+                ((EntityWitherExtend) this.myWither).meap$setIsDoingSpecialAttack(true);
+            }
         }
 
-        // Play summoning sound
-        myWither.worldObj.playSoundEffect(myWither.posX, myWither.posY, myWither.posZ, "mob.wither.idle", 2F, 0.25F);
-    }
-
-    @Override
-    public void updateTask() {
-        if (summonProcessCounter <= 0) return;
-
-        summonProcessCounter--;
-        myWither.getNavigator().clearPathEntity(); // Clear pathing while summoning
-
-        // Optional visual effect loop while summoning
-        if (summonProcessCounter > 0) {
-            for (int i = 0; i < MAX_SUMMONS; i++) {
-                if (summonY[i] > 0) {
-                    for (int j = 0; j < 8; j++) {
-                        myWither.worldObj.playAuxSFX(MEAEffectManager.WITHER_SUMMON_EFFECT_ID, summonX[i], summonY[i], summonZ[i], j);
+        public void updateTask() {
+            if (summonY[0] > 0 || summonY[1] > 0 || summonY[2] > 0) {
+                this.summonProcessCounter--;
+                this.myWither.getNavigator().clearPathEntity();
+                if (this.summonProcessCounter > 0) {
+                    for (int i = 0; i < 3; i++) {
+                        if (summonY[i] > 0) {
+                            for (int j = 0; j < 8; j++) {
+                                this.myWither.worldObj.playAuxSFX(MEAEffectManager.WITHER_SUMMON_EFFECT_ID, summonX[i], summonY[i], summonZ[i], j);
+                            }
+                        }
+                    }
+                } else if (this.summonProcessCounter == 0) {
+                    for (int i = 0; i < 3; i++) {
+                        if (summonY[i] > 0) {
+                            EntitySkeleton summonedSkeleton = (EntitySkeleton) EntityList.createEntityByName("WitherSkeleton", this.myWither.worldObj);
+                            summonedSkeleton.setSkeletonType(1); // Ensure it’s the Wither Skeleton variant
+                            summonedSkeleton.setLocationAndAngles(summonX[i] + 0.5D, summonY[i], summonZ[i] + 0.5D, this.myWither.rand.nextFloat() * 360F, 0.0F);
+                            if (this.myWither.rand.nextInt(3) == 0) {
+                                summonedSkeleton.setCurrentItemOrArmor(0, new ItemStack(BTWItems.steelSword));
+                            }
+                            summonedSkeleton.spawnerInitCreature();
+                            this.myWither.worldObj.playAuxSFX(2004, this.summonX[i], this.summonY[i], this.summonZ[i], 0);
+                            this.myWither.worldObj.spawnEntityInWorld(summonedSkeleton);
+                            if (this.myWither.getAttackTarget() != null) summonedSkeleton.setAttackTarget(this.myWither.getAttackTarget());
+                        }
                     }
                 }
-            }
-        }
-
-        // Spawn skeletons at end of summoning
-        if (summonProcessCounter == 0) {
-            for (int i = 0; i < MAX_SUMMONS; i++) {
-                if (summonY[i] <= 0) continue;
-
-                EntitySkeleton sk = new EntitySkeleton(myWither.worldObj);
-                sk.setSkeletonType(1);
-                sk.setLocationAndAngles(summonX[i] + 0.5D, summonY[i], summonZ[i] + 0.5D, myWither.rand.nextFloat() * 360F, 0);
-
-                if (myWither.rand.nextInt(3) == 0) {
-                    sk.setCurrentItemOrArmor(0, new ItemStack(BTWItems.steelSword));
-                }
-
-                sk.spawnerInitCreature();
-                myWither.worldObj.spawnEntityInWorld(sk);
-
-                EntityLivingBase target = myWither.getAttackTarget();
-                if (target != null) sk.setAttackTarget(target);
-
-                myWither.worldObj.playAuxSFX(2004, summonX[i], summonY[i], summonZ[i], 0);
+            } else {
+                this.summonProcessCounter = -1;
             }
         }
     }
-
-    /**
-     * Find the first valid Y for a skeleton to spawn at a given X,Z near the Wither.
-     */
-    private int findValidGroundY(int x, int baseY, int z) {
-        for (int offset = 0; offset <= 8; offset++) {
-            int y = baseY - offset;
-            if (canSpawnAt(x, y, z)) return y;
-        }
-        for (int offset = 1; offset <= 6; offset++) {
-            int y = baseY + offset;
-            if (canSpawnAt(x, y, z)) return y;
-        }
-        return -1;
-    }
-
-    private boolean canSpawnAt(int x, int y, int z) {
-        EntitySkeleton temp = new EntitySkeleton(myWither.worldObj);
-        temp.setSkeletonType(1);
-        temp.setPosition(x + 0.5D, y, z + 0.5D);
-
-        return myWither.worldObj.checkNoEntityCollision(temp.boundingBox) &&
-                myWither.worldObj.getCollidingBoundingBoxes(temp, temp.boundingBox).isEmpty() &&
-                myWither.worldObj.doesBlockHaveSolidTopSurface(x, y - 1, z);
-    }
-}
